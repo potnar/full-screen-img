100fb7407ab87a139e9e800cbd411425b9b818a4
potnar <potnar6@users.noreply.github.com>
2020-02-11 00:11:17 +0100

    update PureComponent v002

--- src/components/FullscreenImg/index.js
+++ src/components/FullscreenImg/index.js
@@ -3,21 +3,32 @@ import "./FullscreenImg.scss";
 import komp from "../../assets/komp.svg";
 
 class FullscreenImg extends React.PureComponent {
-  constructor(){
+  constructor(props) {
     super(props);
-  this.state = { active: false };
+    this.state = { active: this.props.state };
   }
 
-static getDerivedStateFromProps(nextProps, nextState) {
- if (nextProps.active !== this.state.active) { return ({ active: nextProps.active }); 
-}
-
+  static getDerivedStateFromProps(nextProps, nextState) {
+    if (nextProps.showHide !== this.state.active) {
+      return { active: nextProps.showHide };
+    }
+  }
   render() {
     return (
       <div>
         <button onClick={this.getDerivedStateFromProps}>Click me</button>
-        <div onClick={this.getDerivedStateFromProps ? ".img__backdrop" : ".active-img__backdrop"} className={imgBackdropClasses}>
-          <img className={this.getDerivedStateFromProps ? ".img" : ".active-img"} src={komp} />
+        <div
+          onClick={this.getDerivedStateFromProps}
+          className={
+            this.getDerivedStateFromProps
+              ? ".img__backdrop"
+              : ".active-img__backdrop"
+          }
+        >
+          <img
+            className={this.getDerivedStateFromProps ? ".img" : ".active-img"}
+            src={komp}
+          />
         </div>
       </div>
     );
